f29cdec3bead8189ba250ceb1940a5c8ab1b33e4
Jhojan Restrepo Pareja <jhojanr11.jr@gmail.com>
2025-01-13 16:38:33 -0500

    First react workplace, still in progress

---
+++ bitacora-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import SideBar from './comp/sidebar.js'
+
+function sheet() {
+  return(
+    <div>
+      <p>WORKING</p>
+      <div>
+        <SideBar />
+      </div>
+    </div>
+  );
+};
+
+
+export default sheet;
---
+++ bitacora-react/src/comp/sidebar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function SideBar(){
+
+        const logAlert = () => {
+            console.log("WORKING")
+
+    }
+    return(
+        <div>
+            <h3>Side Bar</h3>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+    </div>
+    ); 
+};
+
+export default SideBar;
